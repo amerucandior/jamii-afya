cba440ddea390568b327438fbe0315332e4bb1a1
gilajb <cjoybett@gmail.com>
2026-03-27 08:52:31 -0700

    modified:   backend/JamiiAfya/asgi.py 	modified:   backend/JamiiAfya/settings.py 	modified:   backend/JamiiAfya/urls.py 	modified:   backend/JamiiAfya/wsgi.py

--- backend/JamiiAfya/settings.py
+++ backend/JamiiAfya/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'JamiiAfyaApp',
+    'JamiiAfyaApp', #Add the app to the installed apps
 ]
 
 MIDDLEWARE = [
