38a92253b7a83c73d02531d07933bddc0223e7e2
ianmwirigi <ianmwirigi@outlook.com>
2026-04-03 20:59:29 +0300

    chore: drop compiled __pycache__ files from repository

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -3,6 +3,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-app = Celery('jamii_fund')
+app = Celery('jamii_afya')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
